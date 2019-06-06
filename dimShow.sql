d5443e74629715a3fd847acf4a886f6d7f2309d8
benhpage <51425425+benhpage@users.noreply.github.com>
2019-06-06 12:44:28 -0500

    -renamed table to dimShow, added ShowNight column

--- dimShow.sql
+++ dimShow.sql
@@ -1,18 +1,18 @@
-/****** Object:  Table [dbo].[dimArtist]    Script Date: 6/4/2019 2:28:32 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[dimSetlist](
+CREATE TABLE [dbo].[dimShow](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[SourceId] [varchar](max) NULL,
 	[ArtistId] int NULL,
 	[ArtistName] [varchar](max) NULL,
 	[VenueId] int NULL,
 	[VenueName] [varchar](max) NULL,
-	[Date] date NULL	
+	[Date] date NULL,
+	[ShowNight] int NULL
 PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
